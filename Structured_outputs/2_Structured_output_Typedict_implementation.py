22601179fd703472bbc3844dacf4e6cbe28aa89b
Prasoon005 <katiyarprasoon003@gmail.com>
2026-06-22 12:53:22 +0530

    implemented the structured_output using Typedict

---
+++ Structured_outputs/2_Structured_output_Typedict_implementation.py
@@ -0,0 +1,29 @@
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+from typing import TypedDict 
+from pathlib import Path
+
+env_path = Path(__file__).resolve.parent.parent / ".env" 
+
+load_dotenv(env_path)
+
+model  = ChatOpenAI()
+
+#Schema 
+
+class Review(TypedDict):
+    summary:str
+    sentiment:str
+
+
+structured_model  =  model.with_structured_output(Review)
+
+
+result  = structured_model.invoke("""The hardware is great, but the software feels bloated. There are
+too many pre-installed apps that I can't remove. Also, the UI looks outdated compared to
+other brands. Hoping for a software update to fix this.""")
+
+print(result)
+
+print(result['summary'])
+print(result['sentiment'])

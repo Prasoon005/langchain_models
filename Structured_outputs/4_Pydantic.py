d562b678ae18704a837ebe8821ebae71e0ac80d2
Prasoon005 <katiyarprasoon003@gmail.com>
2026-06-28 15:15:40 +0530

    Pydantic_basics

--- Structured_outputs/4_Pydantic.py
+++ Structured_outputs/4_Pydantic.py
@@ -1,10 +1,10 @@
-from pydantic import BaseModel, Field , EmailStr
+from pydantic import BaseModel, Field, EmailStr
 from typing import Optional
 
 class Student(BaseModel):
     name: str = "Saksham"
-    age: Optional[int] = None  # age is optional; if not provided, it defaults to None
-    email : EmailStr
+    age: Optional[int] = None
+    email: EmailStr
     cgpa: float = Field(
         gt=0,
         lt=10,
@@ -15,10 +15,21 @@ class Student(BaseModel):
 new_student = {
     "name": "Prasoon",
     "age": "21",
-    "email":"abc@gmail.com",
+    "email": "abc@gmail.com",
     "cgpa": 8.1
 }
 
 student = Student(**new_student)
 
 print(student)
+
+# Convert to Dictionary
+student_dict = student.model_dump()
+
+print(student_dict)
+print(student_dict["age"])
+
+# Convert to JSON
+student_json = student.model_dump_json()
+
+print(student_json)

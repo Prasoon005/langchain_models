3c2092aad9ea886be2cfa4e243d3b9279b460524
Prasoon005 <katiyarprasoon003@gmail.com>
2026-09-17 18:13:36 +0530

    Implemented the Parallel runnables

---
+++ Runnables/Parallel_runnables.py
@@ -0,0 +1,38 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from langchain_core.runnables import RunnableSequence , RunnableParallel 
+
+model  =  ChatGoogleGenerativeAI(
+    model="gemini-2.5-flash",
+    api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+prompt1 = PromptTemplate(
+    template="Generate a tweet on topic : {topic}" , 
+    input_variables=["topic"]
+)
+
+prompt2 = PromptTemplate(
+    template="Generate a linkendIn post for the topic : {topic}",
+    input_variables=['topic']
+)
+
+parser= StrOutputParser() ; 
+
+parallel_chain = RunnableParallel(
+    {
+        'tweet' : RunnableSequence(prompt1 , model , parser) , 
+        'LinkedIn' : RunnableSequence(prompt2 , model , parser)
+    }
+)
+
+result  = parallel_chain.invoke({'topic':'AI'}) 
+
+print(result)   # this would return the result in dictionary format 
+
+print(result['tweet']) 
+
+print(result['LinkedIn'])
